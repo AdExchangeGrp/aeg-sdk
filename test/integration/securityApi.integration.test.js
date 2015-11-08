8f57397758e8502f42b357815ae152acd4c4312c
Justin Durham <jdurham@Justins-MacBook-Pro-2.local>
2015-11-08 17:10:34 -0500

    Tests

--- test/integration/securityApi.integration.test.js
+++ test/integration/securityApi.integration.test.js
@@ -10,8 +10,26 @@ describe('securityApi', () => {
 
 			it('should return without error', (done) => {
 				SecurityApi.testApi.test('Justin').asCallback((err, result) => {
+					if (err) {
+						return done(err);
+					}
 					result.body.should.be.eql({message: 'Hello, Justin!'});
 					done(err);
+				});
+			});
+
+		});
+
+		describe('#testScopeProtected()', () => {
+
+			it('should return with 401', (done) => {
+				SecurityApi.testApi.testScopeProtected('Justin').asCallback((err) => {
+					if (err) {
+						err.response.statusCode.should.be.equal(403);
+						return done();
+					} else {
+						done(new Error('Call should have failed unauthorized'));
+					}
 				});
 			});
 
