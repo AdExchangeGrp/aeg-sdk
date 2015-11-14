4d2a1e09cc617b9ac3922cb488488716afa4ade3
Justin Durham <jdurham@Justins-MacBook-Pro-2.local>
2015-11-14 15:48:33 -0500

    Tests

--- test/integration/securityApi.account.integration.test.js
+++ test/integration/securityApi.account.integration.test.js
@@ -3,7 +3,7 @@
 let securityApi = require('../../lib/api/securityApi.js');
 let ApiError = require('../../lib/errors/apiError.js');
 
-describe('securityApi', () => {
+describe('securityApi - Account', () => {
 
 	let adminPasswordToken;
 	let adminRefreshToken;
