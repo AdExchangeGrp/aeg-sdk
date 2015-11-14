d00852e6899dd249e79f2b1a06bc372714094347
Justin Durham <jdurham@Justins-MacBook-Pro-2.local>
2015-11-14 18:48:50 -0500

    Tests

---
+++ test/integration/securityApi.organization.integration.test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let securityApi = require('../../lib/api/securityApi.js');
+//let ApiError = require('../../lib/errors/apiError.js');
+
+describe('securityApi - Organization', () => {
+
+});
