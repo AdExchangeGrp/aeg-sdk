f8feb37664773c05244dea67e0b92ff5e7f78b30
Justin Durham <jdurham@Justins-MacBook-Pro-2.local>
2015-11-16 14:56:41 -0500

    Babel stuff for tests

--- src/api/securityApi.js
+++ src/api/securityApi.js
@@ -8,7 +8,6 @@ let securityService = null;
 if (config.has('securityService')) {
 	let securityServiceConfig = config.get('securityService');
 	securityService = new SecurityService(securityServiceConfig.host + securityServiceConfig.baseUrl);
-	console.log(securityService);
 }
 
 export default securityService;
