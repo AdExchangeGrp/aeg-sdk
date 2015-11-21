f09a98fe6028b5217f638cb28e25ad2360484471
Justin Durham <jdurham@Justins-MacBook-Pro-2.local>
2015-11-21 09:21:19 -0500

    Cleanup junk

--- src/express/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import login from './login';
-import logout from './logout';
-import googleOAuthCallback from './googleOAuthCallback';
-
-export {login, logout,googleOAuthCallback};
-
-export default {login, logout,googleOAuthCallback};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 'use strict';
 
 import api from './api';
-import express from './express';
 import stormpath from './stormpath';
 import errors from './errors';
 import swagger from './swagger';
 
-export { api, express, stormpath, errors, swagger };
+export { api, stormpath, errors, swagger };
+
+export default {api, stormpath, errors, swagger};
