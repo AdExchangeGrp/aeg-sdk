65a319666d8f332d7370f78f39558cb8ba025191
Justin Durham <jdurham@Justins-MacBook-Pro-2.local>
2015-11-21 11:00:31 -0500

    Remove express stormpath

--- src/stormpath/index.js
+++ src/stormpath/index.js
@@ -4,7 +4,8 @@ import accountUtil from './accountUtil';
 import organizationUtil from './organizationUtil';
 import AffiliateCustomData from './affiliateCustomData';
 import token from './token';
+import stormpathMiddleware from './stormpathMiddleware';
 
-export {accountUtil, organizationUtil, AffiliateCustomData, token};
+export {accountUtil, organizationUtil, AffiliateCustomData, token, stormpathMiddleware};
 
-export default {accountUtil, organizationUtil, AffiliateCustomData, token};
+export default {accountUtil, organizationUtil, AffiliateCustomData, token, stormpathMiddleware};
---
+++ src/stormpath/stormpathMiddleware.js
@@ -0,0 +1,28 @@
+'use strict';
+
+import stormpath from 'stormpath';
+import config from 'config';
+
+export default (app) => {
+
+	const stormpathConfig = config.get('stormpath');
+
+	app.set('stormpathConfig', stormpathConfig);
+
+	const client = new stormpath.Client(stormpathConfig);
+
+	app.set('stormpathClient', client);
+
+	client.getApplication(stormpathConfig.application.href, (err, application) => {
+		if (err) {
+			throw err;
+		}
+
+		app.set('stormpathApplication', application);
+		app.emit('stormpath.ready');
+	});
+
+	return (req, res, next) => {
+		next();
+	};
+};
