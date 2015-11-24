b976b121c7bfe53d9cd127501597a63739ed74a0
Justin Durham <jdurham@Justins-MacBook-Pro-2.local>
2015-11-24 18:16:46 -0500

    remove old security handler

--- src/swagger/index.js
+++ src/swagger/index.js
@@ -1,10 +1,9 @@
 'use strict';
 
-import oauthSecurityHandler from './oauthSecurityHandler';
 import compileSwaggerFile from './compileSwaggerFile';
 import errorResponse from './errorResponse';
 import UnauthorizedError from './unauthorizedError';
 
-export {oauthSecurityHandler, errorResponse, UnauthorizedError, compileSwaggerFile};
+export {errorResponse, UnauthorizedError, compileSwaggerFile};
 
-export default {oauthSecurityHandler, errorResponse, UnauthorizedError, compileSwaggerFile};
+export default {errorResponse, UnauthorizedError, compileSwaggerFile};
