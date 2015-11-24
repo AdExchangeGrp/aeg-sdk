b13e9c27bffc3a82aeea36763f3ec92a60cc86e8
Justin Durham <jdurham@Justins-MacBook-Pro-2.local>
2015-11-24 18:37:29 -0500

    Add security service swagger handler

--- src/swagger/index.js
+++ src/swagger/index.js
@@ -3,7 +3,8 @@
 import compileSwaggerFile from './compileSwaggerFile';
 import errorResponse from './errorResponse';
 import UnauthorizedError from './unauthorizedError';
+import securityHandler from './securityHandler';
 
-export {errorResponse, UnauthorizedError, compileSwaggerFile};
+export {errorResponse, UnauthorizedError, compileSwaggerFile, securityHandler};
 
-export default {errorResponse, UnauthorizedError, compileSwaggerFile};
+export default {errorResponse, UnauthorizedError, compileSwaggerFile, securityHandler};
