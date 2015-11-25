03d900fc1d910d2d9ae75f95d9cc64c88e274228
Justin Durham <jdurham@Justins-MacBook-Pro-2.local>
2015-11-24 19:21:43 -0500

    Add middleware

--- src/swagger/index.js
+++ src/swagger/index.js
@@ -4,7 +4,8 @@ import compileSwaggerFile from './compileSwaggerFile';
 import errorResponse from './errorResponse';
 import UnauthorizedError from './unauthorizedError';
 import securityHandler from './securityHandler';
+import securityMiddleware from './securityMiddleware';
 
-export {errorResponse, UnauthorizedError, compileSwaggerFile, securityHandler};
+export {errorResponse, UnauthorizedError, compileSwaggerFile, securityHandler, securityMiddleware};
 
-export default {errorResponse, UnauthorizedError, compileSwaggerFile, securityHandler};
+export default {errorResponse, UnauthorizedError, compileSwaggerFile, securityHandler, securityMiddleware};
