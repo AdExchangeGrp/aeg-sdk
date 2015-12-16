bd62007103231d34fa9a52f0eb712915611742c0
Justin Durham <justin.durham@icloud.com>
2015-12-16 13:48:43 -0500

    Fix tests

--- test/integration/security/securityApi.account.integration.test.js
+++ test/integration/security/securityApi.account.integration.test.js
@@ -149,8 +149,6 @@ describe('securityApi - Account', () => {
 					result.body.account.customData.should.have.properties(['test', 'testUpdate', 'org']);
 					result.body.account.customData.test.should.be.equal('test');
 					result.body.account.customData.testUpdate.should.be.equal('test-me');
-					//todo:fix me
-					console.log(result.body.account.customData);
 					result.body.account.customData.org.should.have.properties(['href', 'id']);
 					result.body.account.customData.org.should.eql({href: 'test', id: 'test'});
 					done();
