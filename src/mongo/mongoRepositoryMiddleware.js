0f4a92b29cce4247240c8b55b0787fb0815ef211
Justin Durham <justin.durham@icloud.com>
2015-12-17 12:10:19 -0500

    Update mongo

--- src/mongo/mongoRepositoryMiddleware.js
+++ src/mongo/mongoRepositoryMiddleware.js
@@ -1,23 +1,29 @@
 'use strict';
 
 import MongoRepository from './mongoRepository';
+import logger from '@adexchange/aeg-logger';
 
 /**
  * Express middleware for managing a Mongo repository
  * @param {Application} app
+ * @param {function} callback
  * @returns {Function}
  */
-export default (app) => {
+export default (app, callback) => {
 
 	let repository = new MongoRepository();
 
 	repository.connect((err) => {
+
 		if (err) {
-			throw err;
+			return callback(err);
 		}
 
 		app.set('mongoRepository', repository);
-		app.emit('mongo.ready');
+
+		logger.info('mongodb ready...');
+
+		callback();
 	});
 
 	return function (req, res, next) {
