9c0693eec12f885ff98f675661df71b0da3a6252
Justin Durham <justin.durham@icloud.com>
2015-12-17 16:34:03 -0500

    Updates

--- src/swagger/fittings/aeg_authorize.js
+++ src/swagger/fittings/aeg_authorize.js
@@ -18,13 +18,16 @@ export default () => {
 	const stormpathConfig = config.get('stormpath');
 
 	return (context, callback) => {
+
 		const operation = context.request.swagger.operation;
+
 		if (operation['x-aeg-authorize']) {
+
 			var authorize = operation['x-aeg-authorize'];
+			let adminScopes = _.pluck(appConfig.authorizations.adminScopes, 'name');
+
 			switch (authorize.type) {
 				case 'adminOrOwner':
-
-					let adminScopes = _.pluck(appConfig.authorizations.adminScopes, 'name');
 
 					jwt.verify(
 						token.parseTokenFromAuthorization(context.request.headers.authorization),
@@ -44,6 +47,39 @@ export default () => {
 									var resourceId = parseParam(context.request, authorize.parameter);
 									if (resourceId === context.request.account.href) {
 										callback();
+									} else {
+										callback(new PermissionDeniedError());
+									}
+								}
+							}
+						});
+					break;
+				case 'affiliate':
+
+					let affiliateId = parseParam(context.request, authorize.parameter);
+
+					jwt.verify(
+						token.parseTokenFromAuthorization(context.request.headers.authorization),
+						stormpathConfig.apiKey.secret,
+						(err, expandedJwt) => {
+							if (err) {
+								if (err.message === 'Jwt is expired') {
+									callback(new UnauthorizedError(expiredToken));
+								} else {
+									callback(new UnauthorizedError(invalidToken));
+								}
+							} else {
+								let tokenScopes = token.parseScopesFromJwt(expandedJwt);
+
+								if (_.intersection(adminScopes, tokenScopes).length) {
+									callback();
+								} else {
+									if (expandedJwt.body.organization) {
+										if (affiliateId === expandedJwt.body.organization.nameKey) {
+											callback();
+										} else {
+											callback(new PermissionDeniedError());
+										}
 									} else {
 										callback(new PermissionDeniedError());
 									}
