b4d1288aeebf143ae7d35e0d7ae8ccaf136e38b8
Justin Durham <justin.durham@icloud.com>
2016-01-02 12:53:01 -0500

    Logger updates

--- src/api/token.js
+++ src/api/token.js
@@ -9,7 +9,6 @@ import ApiError from './apiError';
 class Token extends EventEmitter {
 
 	fetch(app, callback) {
-		let appConfig = config.get('app');
 		let accessToken = app.get('accessToken');
 
 		if (accessToken) {
@@ -18,48 +17,49 @@ class Token extends EventEmitter {
 				.then(() => {
 					token.willExpire(accessToken, 30, (err) => {
 						if (err) {
-							this.emit('debug', 'service level api token will expire');
-							refreshToken(callback);
+							this.emit('debug', {message: 'service level api token will expire'});
+							this.refreshToken(app, callback);
 						} else {
 							callback(null, accessToken);
 						}
 					});
 				})
 				.fail(() => {
-					this.emit('debug', 'service level api token has expired');
-					refreshToken(callback);
+					this.emit('debug', {message: 'service level api token has expired'});
+					this.refreshToken(app, callback);
 				});
 
 		} else {
-			this.emit('debug', 'service level api token not found');
-			refreshToken(callback);
+			this.emit('debug', {message: 'service level api token not found'});
+			this.refreshToken(app, callback);
 		}
+	}
 
-		function refreshToken(callback) {
+	refreshToken(app, callback) {
 
-			this.emit('debug', 'refresh service level api token');
+		let appConfig = config.get('app');
 
-			securityApi.apiToken({
-					Authorization: 'Basic ' + new Buffer(appConfig.apiKey.id + ':' + appConfig.apiKey.secret).toString('base64'),
-					grantType: 'client_credentials',
-					scope: 'affiliate:service'
-				})
-				.then((result) => {
-					app.set('accessToken', result.body.accessToken);
-					callback(null, result.body.accessToken);
-				})
-				.fail((err) => {
-					this.emit('error', 'failed to refresh service level api token', err);
-					callback(err);
-				});
-		}
+		this.emit('debug', {message: 'refresh service level api token'});
 
+		securityApi.apiToken({
+				Authorization: 'Basic ' + new Buffer(appConfig.apiKey.id + ':' + appConfig.apiKey.secret).toString('base64'),
+				grantType: 'client_credentials',
+				scope: 'affiliate:service'
+			})
+			.then((result) => {
+				app.set('accessToken', result.body.accessToken);
+				callback(null, result.body.accessToken);
+			})
+			.fail((err) => {
+				this.emit('error', {message: 'failed to refresh service level api token', data: err});
+				callback(err);
+			});
 	}
 
 	callApi(app, api, apiCall, apiCallOptions, callback) {
 		this.fetch(app, (err, token) => {
 			if (err) {
-				this.emit('error', 'Could not get api token');
+				this.emit('error', {message: 'Could not get api token', data: err});
 				callback(err);
 			} else {
 				api.setToken(token);
--- src/swagger/fittings/aeg_cache.js
+++ src/swagger/fittings/aeg_cache.js
@@ -1,44 +1,58 @@
 'use strict';
 
 import config from 'config';
-import { logger } from '../../logger-facade';
 import cache from 'express-redis-cache';
+import { EventEmitter } from 'events';
 
 /**
  * Swagger bagpipes fitting to cache api method responses
  * @param {string} cachePrefix
  * @returns {Function}
  */
-export default (cachePrefix) => {
+class Cache extends EventEmitter {
 
-	const appConfig = config.get('app');
-
-	const options = {
-		host: appConfig.cache.host,
-		port: appConfig.cache.port
-	};
-
-	if (cachePrefix) {
-		options.prefix = cachePrefix;
+	constructor(cachePrefix) {
+		super();
+		this._cachePrefix = cachePrefix;
 	}
 
-	const client = cache(options);
+	fitting() {
 
-	return (context, callback) => {
-		const operation = context.request.swagger.operation;
-		if (operation['x-cache']) {
-			logger.debug('aeg_cache: cache hit', {
-				operation: operation.operationId,
-				description: operation.description
-			});
+		const appConfig = config.get('app');
 
-			const options = {
-				expire: operation['x-cache']
-			};
+		const options = {
+			host: appConfig.cache.host,
+			port: appConfig.cache.port
+		};
 
-			client.route(options)(context.request, context.response, callback);
-		} else {
-			callback();
+		if (this._cachePrefix) {
+			options.prefix = this._cachePrefix;
 		}
-	};
-};
+
+		const client = cache(options);
+
+		return (context, callback) => {
+			const operation = context.request.swagger.operation;
+			if (operation['x-cache']) {
+
+				this.emit('debug', {
+					message: 'cache hit',
+					data: {
+						operation: operation.operationId,
+						description: operation.description
+					}
+				});
+
+				const options = {
+					expire: operation['x-cache']
+				};
+
+				client.route(options)(context.request, context.response, callback);
+			} else {
+				callback();
+			}
+		};
+	}
+}
+
+export default Cache;
