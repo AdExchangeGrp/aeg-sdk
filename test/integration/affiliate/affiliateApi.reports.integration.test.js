6876c3f24e22854d5b310340ffc1ba74b3452e15
Justin Durham <justin.durham@icloud.com>
2016-01-09 13:40:34 -0500

    Update tests and proxy

--- test/integration/affiliate/affiliateApi.reports.integration.test.js
+++ test/integration/affiliate/affiliateApi.reports.integration.test.js
@@ -62,19 +62,19 @@ describe('affiliateApi - Reports', () => {
 		describe('#reportsPerformance', () => {
 
 			it('should return performance report daily', (done) => {
-				testPerformaceReport({interval: 'daily', sort: 'clicks'}, 'reportsPerformanceHP', done);
+				testPerformanceReport({interval: 'daily', sort: 'clicks'}, 'reportsPerformanceHP', done);
 			});
 
 			it('should return performance report weekly', (done) => {
-				testPerformaceReport({interval: 'weekly', sort: 'sales'}, 'reportsPerformanceHP', done);
+				testPerformanceReport({interval: 'weekly', sort: 'sales'}, 'reportsPerformanceHP', done);
 			});
 
 			it('should return performance report monthly', (done) => {
-				testPerformaceReport({interval: 'monthly', sort: 'cr'}, 'reportsPerformanceHP', done);
+				testPerformanceReport({interval: 'monthly', sort: 'cr'}, 'reportsPerformanceHP', done);
 			});
 
 			it('should return performance report yearly', (done) => {
-				testPerformaceReport({
+				testPerformanceReport({
 					interval: 'yearly',
 					sort: 'cr',
 					sortDirection: 'desc'
@@ -82,14 +82,14 @@ describe('affiliateApi - Reports', () => {
 			});
 
 			it('should return performance report daily with timezone', (done) => {
-				testPerformaceReport({
+				testPerformanceReport({
 					interval: 'yearly',
 					timezone: 'America/Los_Angeles'
 				}, 'reportsPerformanceHP', done);
 			});
 
 			it('should return performance report yearly with timezone for account 170001', (done) => {
-				testPerformaceReport({
+				testPerformanceReport({
 					interval: 'yearly',
 					timezone: 'America/Los_Angeles',
 					token: passwordToken170001
@@ -97,7 +97,7 @@ describe('affiliateApi - Reports', () => {
 			});
 
 			it('should not return performance report yearly with timezone for account 170001', (done) => {
-				testPerformaceReport({
+				testPerformanceReport({
 						affiliateId: 170002,
 						interval: 'yearly',
 						timezone: 'America/Los_Angeles',
@@ -193,34 +193,32 @@ describe('affiliateApi - Reports', () => {
 		describe('#reportsPerformance', () => {
 
 			it('should return performance report daily', (done) => {
-				testPerformaceReport({interval: 'daily', sort: 'clicks'}, 'reportsPerformanceAEG', done);
+				testPerformanceReportAEG({interval: 'daily'}, 'reportsPerformanceAEG', done);
 			});
 
 			it('should return performance report weekly', (done) => {
-				testPerformaceReport({interval: 'weekly', sort: 'sales'}, 'reportsPerformanceAEG', done);
+				testPerformanceReportAEG({interval: 'weekly'}, 'reportsPerformanceAEG', done);
 			});
 
 			it('should return performance report monthly', (done) => {
-				testPerformaceReport({interval: 'monthly', sort: 'cr'}, 'reportsPerformanceAEG', done);
+				testPerformanceReportAEG({interval: 'monthly'}, 'reportsPerformanceAEG', done);
 			});
 
 			it('should return performance report yearly', (done) => {
-				testPerformaceReport({
-					interval: 'yearly',
-					sort: 'cr',
-					sortDirection: 'desc'
+				testPerformanceReportAEG({
+					interval: 'yearly'
 				}, 'reportsPerformanceAEG', done);
 			});
 
 			it('should return performance report daily with timezone', (done) => {
-				testPerformaceReport({
+				testPerformanceReportAEG({
 					interval: 'yearly',
 					timezone: 'America/Los_Angeles'
 				}, 'reportsPerformanceAEG', done);
 			});
 
 			it('should return performance report yearly with timezone for account 170001', (done) => {
-				testPerformaceReport({
+				testPerformanceReportAEG({
 					interval: 'yearly',
 					timezone: 'America/Los_Angeles',
 					token: passwordToken170001
@@ -228,13 +226,69 @@ describe('affiliateApi - Reports', () => {
 			});
 
 			it('should not return performance report yearly with timezone for account 170001', (done) => {
-				testPerformaceReport({
+				testPerformanceReportAEG({
 						affiliateId: 170002,
 						interval: 'yearly',
 						timezone: 'America/Los_Angeles',
 						token: passwordToken170001
 					},
 					'reportsPerformanceAEG',
+					(err) => {
+						if (err) {
+							done();
+						} else {
+							done(new Error('Should not have returned report'));
+						}
+					});
+			});
+
+		});
+
+		describe('#reportsPerformanceSubIds', () => {
+
+			it('should return performance report daily', (done) => {
+				testPerformanceSubIdReportAEG({interval: 'daily', sort: 'clicks'}, 'reportsPerformanceSubIdsAEG', done);
+			});
+
+			it('should return performance report weekly', (done) => {
+				testPerformanceSubIdReportAEG({interval: 'weekly', sort: 'sales'}, 'reportsPerformanceSubIdsAEG', done);
+			});
+
+			it('should return performance report monthly', (done) => {
+				testPerformanceSubIdReportAEG({interval: 'monthly', sort: 'cr'}, 'reportsPerformanceSubIdsAEG', done);
+			});
+
+			it('should return performance report yearly', (done) => {
+				testPerformanceSubIdReportAEG({
+					interval: 'yearly',
+					sort: 'cr',
+					sortDirection: 'desc'
+				}, 'reportsPerformanceSubIdsAEG', done);
+			});
+
+			it('should return performance report daily with timezone', (done) => {
+				testPerformanceSubIdReportAEG({
+					interval: 'yearly',
+					timezone: 'America/Los_Angeles'
+				}, 'reportsPerformanceSubIdsAEG', done);
+			});
+
+			it('should return performance report yearly with timezone for account 170001', (done) => {
+				testPerformanceSubIdReportAEG({
+					interval: 'yearly',
+					timezone: 'America/Los_Angeles',
+					token: passwordToken170001
+				}, 'reportsPerformanceSubIdsAEG', done);
+			});
+
+			it('should not return performance report yearly with timezone for account 170001', (done) => {
+				testPerformanceSubIdReportAEG({
+						affiliateId: 170002,
+						interval: 'yearly',
+						timezone: 'America/Los_Angeles',
+						token: passwordToken170001
+					},
+					'reportsPerformanceSubIdsAEG',
 					(err) => {
 						if (err) {
 							done();
@@ -360,7 +414,37 @@ describe('affiliateApi - Reports', () => {
 			});
 	}
 
-	function testPerformaceReport(options, func, callback) {
+	function testPerformanceReportAEG(options, func, callback) {
+		affiliateApi.setToken(options.token ? options.token : adminPasswordToken);
+
+		var args = {
+			affiliateId: options.affiliateId ? options.affiliateId : 170001,
+			interval: options.interval ? options.interval : 'daily',
+			filter: 'all'
+		};
+
+		if (options.timezone) {
+			args.timezone = options.timezone;
+		}
+
+		affiliateApi[func](args)
+			.then((result) => {
+				validatePerformanceReportAEG(result);
+				callback();
+			})
+			.fail((err) => {
+				callback(err);
+			});
+	}
+
+	function validatePerformanceReportAEG(result) {
+		should.exist(result);
+		should.exist(result.body);
+		should.exist(result.body.data);
+		result.body.data.should.have.properties(['revenue', 'epc', 'sales', 'clicks', 'cr']);
+	}
+
+	function testPerformanceReport(options, func, callback) {
 		affiliateApi.setToken(options.token ? options.token : adminPasswordToken);
 
 		var args = {
@@ -394,6 +478,90 @@ describe('affiliateApi - Reports', () => {
 		should.exist(result.body);
 		should.exist(result.body.data);
 		result.body.data.should.have.properties(['revenue', 'epc', 'sales', 'clicks', 'cr', 'subIds']);
+		_.isArray(result.body.data.subIds).should.be.ok;
+
+		var lastVal;
+
+		if (limit) {
+			(result.body.data.subIds.length <= limit).should.be.ok;
+		}
+
+		_.each(result.body.data.subIds, (sub, i) => {
+
+			sub.should.have.properties(['id', 'clicks', 'sales', 'cr']);
+
+			switch (sort) {
+				case 'sales':
+					if (i === 0) {
+						lastVal = sub.sales;
+					}
+					if (sortDirection === 'asc') {
+						(sub.sales >= lastVal).should.be.ok;
+					} else {
+						(sub.sales <= lastVal).should.be.ok;
+					}
+					lastVal = sub.sales;
+					break;
+				case 'clicks':
+					if (i === 0) {
+						lastVal = sub.clicks;
+					}
+					if (sortDirection === 'asc') {
+						(sub.clicks >= lastVal).should.be.ok;
+					} else {
+						(sub.clicks <= lastVal).should.be.ok;
+					}
+					lastVal = sub.clicks;
+					break;
+				case 'cr':
+					if (i === 0) {
+						lastVal = sub.cr;
+					}
+					if (sortDirection === 'asc') {
+						(sub.cr >= lastVal).should.be.ok;
+					} else {
+						(sub.cr <= lastVal).should.be.ok;
+					}
+					lastVal = sub.cr;
+					break;
+			}
+		});
+	}
+
+	function testPerformanceSubIdReportAEG(options, func, callback) {
+		affiliateApi.setToken(options.token ? options.token : adminPasswordToken);
+
+		var args = {
+			affiliateId: options.affiliateId ? options.affiliateId : 170001,
+			interval: options.interval ? options.interval : 'daily',
+			sort: options.sort ? options.sort : 'clicks',
+			sortDirection: options.sortDirection ? options.sortDirection : 'asc',
+			filter: 'all'
+		};
+
+		if (options.timezone) {
+			args.timezone = options.timezone;
+		}
+
+		if (options.limit) {
+			args.limit = options.limit;
+		}
+
+		affiliateApi[func](args)
+			.then((result) => {
+				validatePerformanceSubIdReportAEG(result, args.sort, args.sortDirection, args.limit);
+				callback();
+			})
+			.fail((err) => {
+				callback(err);
+			});
+	}
+
+	function validatePerformanceSubIdReportAEG(result, sort, sortDirection, limit) {
+		should.exist(result);
+		should.exist(result.body);
+		should.exist(result.body.data);
+		result.body.data.should.have.properties(['subIds']);
 		_.isArray(result.body.data.subIds).should.be.ok;
 
 		var lastVal;
