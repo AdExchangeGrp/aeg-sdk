bf409bbc57b274053f706b8afb255b02a65dcfd7
Justin Durham <justin.durham@icloud.com>
2016-02-29 15:41:10 -0500

    Remove mongo code

--- src/mongo/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import MongoRepository from './mongo-repository';
-import MongoRepositoryMiddleware from './mongo-repository-middleware';
-
-export {MongoRepository, MongoRepositoryMiddleware};
-
-export default {MongoRepository, MongoRepositoryMiddleware};
