7ba425e79d13950d58e0db373b52b37f89014c42
Justin Durham <justin.durham@icloud.com>
2016-02-29 16:02:19 -0500

    remove ses

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@
 import api from './api';
 import stormpath from './stormpath';
 import swagger from './swagger';
-import aws from './aws';
 
 export { api, stormpath, swagger };
 
-export default {api, stormpath, swagger, aws};
+export default {api, stormpath, swagger};
