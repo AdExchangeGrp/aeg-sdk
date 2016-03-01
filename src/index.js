4b310b4491d3d67170f7f46891569ad86b780939
Justin Durham <justin.durham@icloud.com>
2016-02-29 19:34:08 -0500

    Remove swagger

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 'use strict';
 
 import api from './api';
-import stormpath from './stormpath';
-import swagger from './swagger';
 
-export { api, stormpath, swagger };
+export {api};
 
-export default {api, stormpath, swagger};
+export default {api};
--- src/swagger/fittings/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import aegAuthorize from './aeg-authorize';
-import aegCache from './aeg-cache';
-
-export {aegAuthorize, aegCache};
-
-export default {aegAuthorize, aegCache};
