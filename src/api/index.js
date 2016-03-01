8613e90a8a140d823b8231ff001d6ce835573a8e
Justin Durham <justin.durham@icloud.com>
2016-02-29 20:01:17 -0500

    Cleanup SDK

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,7 @@
 import securityApi from './security-api';
 import affiliateApi from './affiliate-api';
 import ApiError from './api-error';
-import Token from './token';
 
-export {securityApi, affiliateApi, ApiError, Token};
+export {securityApi, affiliateApi, ApiError};
 
-export default {securityApi, affiliateApi, ApiError, Token};
+export default {securityApi, affiliateApi, ApiError};
