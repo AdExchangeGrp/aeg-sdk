aa6ede50e592233be539bc5db23653c905e3ef20
Justin Durham <justin.durham@icloud.com>
2016-02-29 22:03:08 -0500

    Updates

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,8 @@
 import securityApi from './security-api';
 import affiliateApi from './affiliate-api';
 import ApiError from './api-error';
+import token from './token';
 
-export {securityApi, affiliateApi, ApiError};
+export {securityApi, affiliateApi, ApiError, token};
 
-export default {securityApi, affiliateApi, ApiError};
+export default {securityApi, affiliateApi, ApiError, token};
