52c0a13d1b88a4fa934e3920c6a64d2613b04dd4
Justin Durham <justin.durham@icloud.com>
2016-08-26 12:10:14 -0400

    Config updates

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,10 @@
 
 import securityApi from './security-api';
 import affiliateApi from './affiliate-api';
+import fulfillmentApi from './fulfillment-api';
 import ApiError from './api-error';
 import token from './token';
 
-export {securityApi, affiliateApi, ApiError, token};
+export {securityApi, affiliateApi, fulfillmentApi, ApiError, token};
 
-export default {securityApi, affiliateApi, ApiError, token};
+export default {securityApi, affiliateApi, fulfillmentApi, ApiError, token};
