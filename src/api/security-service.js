73600799b1e646be16f26b58ec4d1f1005a312f6
Justin Durham <justin.durham@icloud.com>
2017-07-26 14:30:02 -0400

    Add password reset

--- src/api/security-service.js
+++ src/api/security-service.js
@@ -1297,6 +1297,115 @@ var SecurityService = (function() {
         return deferred.promise;
     };
     /**
+     * Update an account password as the account owner or with an admin scope
+     * @method
+     * @name SecurityService#updateAccountPassword
+     * @param {string} id - Account href
+     * @param {string} directory - Directory href
+     * @param {string} email - Email address
+     * @param {string} password - Password
+     * 
+     */
+    SecurityService.prototype.updateAccountPassword = function(parameters) {
+        if (parameters === undefined) {
+            parameters = {};
+        }
+        var deferred = Q.defer();
+
+        var domain = this.domain;
+        var path = '/v1/account/password';
+
+        var body;
+        var queryParameters = {};
+        var headers = {};
+        var form = {};
+
+        if (this.token.isQuery) {
+            queryParameters[this.token.headerOrQueryName] = this.token.value;
+        } else if (this.token.headerOrQueryName) {
+            headers[this.token.headerOrQueryName] = this.token.value;
+        } else {
+            var prefix = this.token.prefix ? this.token.prefix : 'Bearer';
+            headers['Authorization'] = prefix + ' ' + this.token.value;
+        }
+
+        if (parameters['id'] !== undefined) {
+            form['id'] = parameters['id'];
+        }
+
+        if (parameters['directory'] !== undefined) {
+            form['directory'] = parameters['directory'];
+        }
+
+        if (parameters['email'] !== undefined) {
+            form['email'] = parameters['email'];
+        }
+
+        if (parameters['password'] !== undefined) {
+            form['password'] = parameters['password'];
+        }
+
+        if (parameters['password'] === undefined) {
+            deferred.reject(new Error('Missing required  parameter: password'));
+            return deferred.promise;
+        }
+
+        if (parameters.$queryParameters) {
+            Object.keys(parameters.$queryParameters)
+                .forEach(function(parameterName) {
+                    var parameter = parameters.$queryParameters[parameterName];
+                    queryParameters[parameterName] = parameter;
+                });
+        }
+
+        var req = {
+            method: 'POST',
+            uri: domain + path,
+            qs: queryParameters,
+            headers: headers,
+            body: body
+        };
+
+        if (typeof(body) === 'object' && !(body instanceof Buffer)) {
+            req.json = true;
+        }
+
+        if (!req.json) {
+            if (Object.keys(form).length > 0) {
+                req.form = form;
+            } else {
+                req.form = {};
+            }
+        }
+
+        request(req, function(error, response, body) {
+            if (error) {
+                deferred.reject(error);
+            } else {
+                if (/^application\/(.*\\+)?json/.test(response.headers['content-type'])) {
+                    try {
+                        body = JSON.parse(body);
+                    } catch (e) {
+
+                    }
+                }
+                if (response.statusCode >= 200 && response.statusCode <= 299) {
+                    deferred.resolve({
+                        response: response,
+                        body: body
+                    });
+                } else {
+                    deferred.reject({
+                        response: response,
+                        body: body
+                    });
+                }
+            }
+        });
+
+        return deferred.promise;
+    };
+    /**
      * Add a scope to an account
      * @method
      * @name SecurityService#addScopeToAccount
